675d51e7e27a9354b79034a64818c4977e229071
Facundo Fanti <107153166+suzukiven0m@users.noreply.github.com>
2026-06-25 11:21:29 +0200

    fix: correct seed data (8 docks, departed ships), dev-only migration, register filters

--- backend/Octopus.Api/Program.cs
+++ backend/Octopus.Api/Program.cs
@@ -1,15 +1,20 @@
 using Microsoft.EntityFrameworkCore;
+using Octopus.Api.Common;
 using Octopus.Api.Data;
 using Octopus.Api.Services;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<ValidationFilter>();
+    options.Filters.Add<ApiExceptionFilter>();
+})
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,7 +26,6 @@ builder.Services.AddScoped<ShipService>();
 builder.Services.AddScoped<DockService>();
 builder.Services.AddScoped<AssignmentService>();
 builder.Services.AddScoped<SystemService>();
-builder.Services.AddScoped<AuthService>();
 
 builder.Services.AddCors(options =>
 {
@@ -33,15 +37,15 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    context.Database.Migrate();
-    SeedData.Initialize(context);
-}
-
 if (app.Environment.IsDevelopment())
 {
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        context.Database.Migrate();
+        SeedData.Initialize(context);
+    }
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
